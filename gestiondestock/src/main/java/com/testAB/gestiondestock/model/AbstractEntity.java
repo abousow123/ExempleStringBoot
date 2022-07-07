d131da254cfec3f5f6272ed1bb6c765db52febe4
abousow123 <abousow798@gmail.com>
2022-07-08 00:54:33 +0200

    MAJ creation entities

--- gestiondestock/src/main/java/com/testAB/gestiondestock/model/AbstractEntity.java
+++ gestiondestock/src/main/java/com/testAB/gestiondestock/model/AbstractEntity.java
@@ -4,8 +4,10 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.Column;
+import javax.persistence.EntityListeners;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
@@ -13,6 +15,7 @@ import java.time.Instant;
 
 @Data
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class AbstractEntity implements Serializable {
 
     @Id
